02779e7167cf3d908f79634dad21f7b3ac9792b7
ajaychandru20 <ajaychandru20@gmail.com>
2025-05-31 23:47:19 +0530

    completed log4j

--- Selenium-Automation-Toolkit/src/main/java/Log4jExamples/PropertyFileConfigClass.java
+++ Selenium-Automation-Toolkit/src/main/java/Log4jExamples/PropertyFileConfigClass.java
@@ -1,4 +1,18 @@
 package Log4jExamples;
 
+import org.apache.log4j.Logger;
+
 public class PropertyFileConfigClass {
+
+    static Logger logger = Logger.getLogger(PropertyFileConfigClass.class);
+
+    public static void main(String[] args) {
+
+            logger.info("This is info logger");
+            logger.debug("This is debugger logger");
+            logger.warn("This is warn logger");
+            logger.error("This is error logger");
+            logger.fatal("This is fatel logger");
+    }
+
 }
